4009e46011b0c4789a5e7d3405343545808830d8
David Ledesma <31101968+DavidZenkay11@users.noreply.github.com>
2024-08-06 20:32:45 -0300

    Update Program.cs
    Hola Mundo -> Chau

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -10,7 +10,7 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo.");
+            Console.WriteLine("Chau.");
             Console.ReadKey();
         }
     }
