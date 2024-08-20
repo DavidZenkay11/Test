ee0eca9565f66f930cacdb081ff840bba57feacd
David Ledesma <31101968+DavidZenkay11@users.noreply.github.com>
2024-08-19 21:47:23 -0300

    tfi

--- ConexionAPI.cs
+++ ConexionAPI.cs
@@ -1,8 +1,25 @@
-﻿using System;
+﻿using RestSharp.Authenticators;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
 
-public class Class1
+namespace WindowsFormsApp1
 {
-	public Class1()
-	{
-	}
+    internal class ConexionAPI
+    {
+        RestRequest request;
+        RestClient client = new RestClient("https://fakestoreapi.com/products");
+        public RestResponse Get(string product)
+        {
+            request = new RestRequest(product,Method.Get);
+        }
+    }
 }
